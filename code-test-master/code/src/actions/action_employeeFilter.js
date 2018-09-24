f9b0d835828763bc2433fc05c34af3eca9daf3d8
kiranz38 <40621168+kiranz38@users.noreply.github.com>
2018-09-24 17:19:44 +0530

    Update action_employeeFilter.js

--- code-test-master/code/src/actions/action_employeeFilter.js
+++ code-test-master/code/src/actions/action_employeeFilter.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import companyData from '../reducers/sample-data';
+import axios from 'axios';
 
 //Author: Ram Date :7/10/2018
 //Reducer for filtering user search and sorting mechanism
 export function filterEmployee(term,sortTerm=null){
-if(!sortTerm){
-  var employeeFilter =JSON.parse(companyData).employees.filter((employee)=>employee.firstName.toLowerCase().includes(term.toLowerCase()));
+
   return ({
-    type:'EMPLOYEE_FILTER',
-    payload: employeeFilter
+    type:'REDUCER_FILTER',
+    payload: employeeSort
   });
-}
-else{
-  var employeeFilter =JSON.parse(companyData).employees.filter((employee)=>employee.firstName.toLowerCase().includes(term.toLowerCase()));
-   var employeeSort = _.sortByOrder(employeeFilter, [sortTerm],['asc']);
-    return ({
-      type:'EMPLOYEE_FILTER',
-      payload: employeeSort
-    });
-}
 
 }
