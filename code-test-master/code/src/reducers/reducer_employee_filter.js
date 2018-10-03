8520aeb33c2b0a966e0831c59d2e464a8744c7be
kiranz38 <40621168+kiranz38@users.noreply.github.com>
2018-10-03 14:42:45 +0530

    Update reducer_employee_filter.js

--- code-test-master/code/src/reducers/reducer_employee_filter.js
+++ code-test-master/code/src/reducers/reducer_employee_filter.js
@@ -1,9 +1,9 @@
 //Author Ram Date: 7/10/2018
 //Action for responding back the payload to components after user search
-export default function(state=null, action){
+export default function(state=[], action){
 switch (action.type) {
-  case 'EMPLOYEE_FILTER':
-  return action.payload;
+  case 'REDUCER_FILTER':
+  return action.payload.data.employees;
 }
 return state;
 }
